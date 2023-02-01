ae8f30f204380e126e557bb437f3aa4f779814b5
hector <hectortoraltrabajos@gmail.com>
2023-02-01 01:24:49 +0100

    debuguear la parte de las CSP

--- app.ts
+++ app.ts
@@ -16,8 +16,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(cors())
-app.use(helmet())
+app.use(cors({
+  origin: ['https://cdn.redoc.ly']
+}))
+app.use(helmet({
+  contentSecurityPolicy: false
+}))
 app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, 'public')))
