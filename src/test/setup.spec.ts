708878f128106cc835e420548f43b737905cab80
Hector Toral <hectortoraltrabajos@gmail.com>
2023-08-29 14:41:33 +0200

    new try setup

--- src/test/setup.spec.ts
+++ src/test/setup.spec.ts
@@ -1,10 +1,9 @@
 import { UserModel } from '@/api_server/models/user'
 
-afterEach((done) => {
-  UserModel.deleteMany({})
-    .then(() => done())
-    .catch((err) => {
-      console.log(err)
-      done()
-    })
+afterEach(async () => {
+  try {
+    await UserModel.deleteMany({})
+  } catch (err) {
+    console.error(err)
+  }
 })
