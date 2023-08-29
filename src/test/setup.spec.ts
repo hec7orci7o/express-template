d6e5efe7e4b0c86164faa6b1ba93112c64cf2c77
Hector Toral <hectortoraltrabajos@gmail.com>
2023-08-29 13:54:51 +0200

    uri

--- src/test/setup.spec.ts
+++ src/test/setup.spec.ts
@@ -1,10 +1,5 @@
 import { UserModel } from '@/api_server/models/user'
 
-beforeEach((done) => {
-  UserModel.deleteMany({}).catch((err) => console.log(err))
-  done()
-})
-
 afterEach((done) => {
   UserModel.deleteMany({}).catch((err) => console.log(err))
   done()
